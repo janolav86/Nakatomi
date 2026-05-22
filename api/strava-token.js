3db8e91555f4889af1a18eeb3705d335f5c5f1f5
Jan-Olav Karlsen <jan@karls1.com>
2026-05-22 23:25:15 +0200

    debug strava token

--- api/strava-token.js
+++ api/strava-token.js
@@ -1,6 +1,10 @@
 export default async function handler(req, res) {
   const { code } = req.query;
 
+  if (!code) {
+    return res.status(400).json({ error: 'No code provided' });
+  }
+
   const response = await fetch('https://www.strava.com/oauth/token', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -14,8 +18,15 @@ export default async function handler(req, res) {
 
   const data = await response.json();
 
-  if (data.errors) {
-    return res.status(400).json({ error: 'Token exchange failed' });
+  // Returner hele svaret så vi kan se hva Strava sier
+  if (data.errors || data.message) {
+    return res.status(400).json({ 
+      error: 'Token exchange failed',
+      strava_message: data.message,
+      strava_errors: data.errors,
+      client_id_set: !!process.env.STRAVA_CLIENT_ID,
+      client_secret_set: !!process.env.STRAVA_CLIENT_SECRET
+    });
   }
 
   res.status(200).json({
